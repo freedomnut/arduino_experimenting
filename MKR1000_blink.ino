82c12a561f1bf48c22d9fa45a1153993168879c9
freedomnut <freedomnut@users.noreply.github.com>
2016-07-25 10:07:32 -0700

    This short blink script should work on the Arduino
    Just commit it!

---
+++ MKR1000_blink.ino
@@ -0,0 +1,15 @@
+const int LEDpin = 6;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(LEDpin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(LEDpin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                  // wait for a second
+  digitalWrite(LEDpin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                  // wait for a second
+}
